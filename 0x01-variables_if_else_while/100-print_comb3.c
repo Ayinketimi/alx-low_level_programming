14e9e0be69372fb495075cbbef600e4c24e0317c
Ayinketimi <Ayinketimi@gmail.com>
2023-06-28 05:52:00 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,30 +6,26 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = '0'; i <= '8'; i++)
- 
-    {
-        for (j = '0'; j <= '9'; j++)
-
-        {
-		if (j > i)
-              {
-		      putchar(i);
-		      putchar(j);
-         	      if (i != '8' || j != '9')
-            
-                      {
-                           putchar(',');
-                           putchar(' ');
-                      } 
-            
-               } 
-        
-          }
-     }	
-    putchar('\n');
-    return (0);
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8' || j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
 
