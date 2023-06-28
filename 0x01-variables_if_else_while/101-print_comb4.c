4a3cde1550f4ddda32ba4fcd1c92e5025a72e20c
Ayinketimi <Ayinketimi@gmail.com>
2023-06-28 05:06:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,32 +6,31 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    i = 0;
-    while (i < 10)
-    {
-        j = i + 1;
-        while (j < 10) 
-        {
-            k = j + 1;
-            while (k < 10) 
-            {
-                putchar(i + '0'); 
-                putchar(j + '0');
-                putchar(k + '0'); 
-                if (i < 7 || j < 8 || k < 9) 
-                {
-                    putchar(',');
-                    putchar(' '); 
-                }
-                k++;
-            }
-            j++; 
-        }
-        i++; 
-    }
-     putchar('\n');
-    return (0);
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7' || j != '8')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
+
 
---
+++ README.md
@@ -0,0 +1,1 @@
+Hello, this is alx-low_level_programming.
