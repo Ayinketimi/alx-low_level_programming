e3aa9d4f87013c22d8adc8565943dd3386c0e5a5
Ayinketimi <Ayinketimi@gmail.com>
2023-06-30 18:41:53 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; i < 10; i++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
-	for (n = 'a'; n <= 'f'; i++)
+	for (n = 'a'; n <= 'f'; n++)
 	{
 		putchar(n);
 	}
