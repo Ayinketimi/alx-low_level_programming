4a8f5deb47c4564f61eff7385c28882620bc8b51
Ayinketimi <Ayinketimi@gmail.com>
2023-07-24 11:11:22 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, l, f, d;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	l = 0;
+	f = 0;
+	d = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	while (a < l && f == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			d = s[a] - '0';
+			if (b % 2)
+				d = -d;
+			c = c * 10 + d;
+			f = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			f = 0;
+		}
+		a++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (c);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, n1, n2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	result = n1 * n2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
