227e0af9d2ddb3a16f455470afa4544b9bf58ab5
Ayinketimi <Ayinketimi@gmail.com>
2023-11-08 21:50:48 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /**
  * check_num - check - string there are digit
  * @str: array str
@@ -10,51 +11,49 @@
  */
 int check_num(char *str)
 {
-        unsigned int count;
+	unsigned int count;
 
-        count = 0;
-        while (count < strlen(str))
-        {
-                if (!isdigit(str[count]))
-                {
-                        return (0);
-                }
-
-                count++;
-        }
-        return (1);
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
 }
 
 /**
  * main - Print the name of the program
  * @argc: Count arguments
- * @argv: Arguments*
+ * @argv: Arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-        int count;
-        int str_to_int;
-        int add = 0;
+	int count;
+	int str_to_int;
+	int add = 0;
 
-        count = 1;
-        while (count < argc)
-        {
-                if (check_num(argv[count]))
-                {
-                        str_to_int = atoi(argv[count]);
-                        add += str_to_int;
-                }
-                else
-                {
-                        printf("Error\n");
-                        return (1);
-                }
-                count++;
-        }
-
-        printf("%d\n", add);
-
-        return (0);
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			add += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum); /*print sum*/
+	return (0);
 }
 
